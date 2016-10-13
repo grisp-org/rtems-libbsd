349580135cb50ba654f88f23066671e74bd61e0f
Christian Mauderer <Christian.Mauderer@embedded-brains.de>
2016-10-13 13:47:57 +0200

    atsam_otg: Reference usbus module.

--- freebsd/sys/dev/usb/controller/usb_controller.c
+++ freebsd/sys/dev/usb/controller/usb_controller.c
@@ -141,6 +141,7 @@ DRIVER_MODULE(usbus, octusb, usb_driver, usb_devclass, 0, 0);
 /* Dual Mode Drivers */
 DRIVER_MODULE(usbus, dwcotg, usb_driver, usb_devclass, 0, 0);
 DRIVER_MODULE(usbus, saf1761otg, usb_driver, usb_devclass, 0, 0);
+DRIVER_MODULE(usbus, ats_otg_7x, usb_driver, usb_devclass, 0, 0);
 
 /*------------------------------------------------------------------------*
  *	usb_probe
